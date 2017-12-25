a164e6f217a7dbf8ac64dbd7cb34f15970a43f12
Hubert Jasudowicz <hubert.jasudowicz@gmail.com>
2017-12-25 01:10:23 +0100

    Style fixes

--- http/app/app.py
+++ http/app/app.py
@@ -1,22 +1,35 @@
+"""
+Main module of HTTP Server
+"""
+import logging
+import sys
+import signal
 import tornado.ioloop
 import tornado.web
 
+def term_handler():
+    """ Handle SIGTERM signal from Docker """
+    sys.exit(0)
 
 class MainHandler(tornado.web.RequestHandler):
+    """ Handler class for incoming HTTP requests """
     def data_received(self, chunk):
         pass
 
-    def get(self):
+    def get(self, *args, **kwargs):
         self.write("Hello, world")
 
-
 def make_app():
+    """ Build main app """
     return tornado.web.Application([
         (r"/", MainHandler),
     ])
 
-
 if __name__ == "__main__":
-    app = make_app()
-    app.listen(5000)
-    tornado.ioloop.IOLoop.current().start()
+    signal.signal(signal.SIGTERM, term_handler)
+    try:
+        app = make_app()
+        app.listen(5000)
+        tornado.ioloop.IOLoop.current().start()
+    finally:
+        logging.info("Exiting...")
